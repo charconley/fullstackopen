92a812d995fab394c0d1c60c552631ba1c5d6270
Charlie Conley <conleyw@uci.edu>
2024-09-18 22:28:12 -0400

    Create Sum.jsx

---
+++ part2/courseInfo/src/Components/Sum.jsx
@@ -0,0 +1,10 @@
+const Sum = ({exercises}) => {
+    return (
+        <div>
+            <h3>
+            total of {exercises} exercises
+           </h3>
+        </div>
+    )
+}
+export default Sum
